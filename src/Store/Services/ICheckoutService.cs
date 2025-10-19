624f290e17b074dad8e9df8349bd858abc2bce09
Bruno Capuano <bcapuano@gmail.com>
2025-10-19 11:25:20 -0400

    delete unused files

--- src/Store/Services/ICheckoutService.cs
+++
@@ -1,9 +0,0 @@
-using CartEntities;
-
-namespace Store.Services;
-
-public interface ICheckoutService
-{
-    Task<Order> ProcessOrderAsync(Customer customer, Cart cart);
-    Task<Order?> GetOrderAsync(string orderNumber);
-}
